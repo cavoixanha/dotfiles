36e87cebbcde1355b9f1b8924b7c3e709a5ad6b3
Nam Nguyen Van <kiemsi789@yahoo.com>
2020-07-14 00:34:45 +0700

    [ADD] soft for macos
    Signed-off-by: Nam Nguyen Van <kiemsi789@yahoo.com>

---
+++ .software/brew.zsh
@@ -0,0 +1,27 @@
+# Make sure we’re using the latest Homebrew.
+brew update
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install Cask
+tap 'caskroom/cask'
+
+brew install ack
+brew install cowsay
+brew install fortune
+brew install figlet
+brew install git
+brew install git-flow
+brew install p7zip
+brew install tmux
+brew install tree
+brew install vim
+brew install zsh
+brew install zsh-completions
+brew install nodejs
+
+
+brew cask install google-chrome
+
+# Remove outdated versions from the cellar.
+brew cleanup
---
+++ .software/mac_install.sh
@@ -0,0 +1,42 @@
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew doctor
+
+# Install zsh
+brew install zsh
+chsh -s /bin/zsh
+
+# Install prezto
+chsh -s $(which zsh)
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+brew install git
+brew install p7zip
+brew install neovim
+brew install rbenv
+
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+brew install yarn
+
+
+brew install zsh-autosuggestions
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
+
+brew install the_silver_searcher
+
+# Install lolcat
+gem install lolcat
+
+
+
+# linked apps
+brew linkapps
+
+# Fix some error
+# compaudit | xargs chmod g-w
+echo "**Memory Clean** ..."
